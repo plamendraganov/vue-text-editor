86fc7596f31f2d2ed83c7043a170f71f71bf5f38
draganov <draganov123@abv.bg>
2018-09-11 10:43:56 +0300

    correction of the code indentation

--- src/App.vue
+++ src/App.vue
@@ -73,31 +73,41 @@
 
       LinkedList.prototype.addToHead = function(value) {
           const newNode = new Node(value, this.head, null);
+
           if(this.head){ 
               this.head.prev = newNode;
           }else{
               this.tail = newNode; 
           }
+
           this.head = newNode;
       };
 
       LinkedList.prototype.addToTail = function(value) {
           const newNode = new Node(value, null, this.tail);
+
           if(this.tail){
               this.tail.next = newNode;
           }else{
               this.head = newNode;
           }
+
           this.tail = newNode;
       }
 
       LinkedList.prototype.removeHead = function() {
-          if (!this.head) return null;
+          if(!this.head){
+              return null;
+          }
+
           let value = this.head.value;
           this.head = this.head.next;
 
-          if (this.head) this.head.prev = null;
-          else this.tail = null;
+          if(this.head){
+              this.head.prev = null;
+          }else{
+              this.tail = null;
+          }
 
           return value;
       }
